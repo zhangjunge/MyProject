505fcaf801f04cc73f021cb04d0f4cafacc8c28b
张俊格 <dove.zhang@vipshop.com>
2015-12-18 10:53:36 +0800

    本地增加system.out

--- src/com/dove/test/FirstTest.java
+++ src/com/dove/test/FirstTest.java
@@ -1,5 +1,9 @@
 package com.dove.test;
 
 public class FirstTest {
+	public static void main(String[] args) {
+		System.out.println("i edit it on github!");
+		System.out.println("i edit it on local！");
+	}
 
 }
