b271c41f084ce5ab62545652eb7344d1b24e2d93
张俊格 <dove.zhang@vipshop.com>
2015-12-18 11:24:24 +0800

    合并

--- src/com/dove/test/MergeTest.java
+++ src/com/dove/test/MergeTest.java
@@ -7,10 +7,16 @@ public class MergeTest {
 		System.out.print("i add something which conflict to github ");
 		System.out.println("i edit it on github");
 		localmethod();
+		githubmethod();
 	}
 	
     static private int localmethod(){
     	System.out.println("本地增加方法");
+    	return 1;
+    }
+	
+	static private int githubmethod(){
+    	System.out.println("github增加方法");
     	return 1;
     }
 
