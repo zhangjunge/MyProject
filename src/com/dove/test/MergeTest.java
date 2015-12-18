7c8b4ae3b0db7df37ddac00127cf6f9e92af185e
张俊格 <dove.zhang@vipshop.com>
2015-12-18 11:11:41 +0800

    本地增加冲突github

--- src/com/dove/test/MergeTest.java
+++ src/com/dove/test/MergeTest.java
@@ -4,6 +4,7 @@ public class MergeTest {
 
 	public static void main(String[] args) {
 		System.out.println("i edit it on local!");
+		System.out.print("i add something which conflict to github ");
 	}
 
 }
