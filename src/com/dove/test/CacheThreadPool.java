c3feb9ba769873a2fe1b29c1e31d9f8f99ce986d
zhangjunge <593450804@qq.com>
2015-12-18 17:36:25 +0800

    测试excutorService

---
+++ src/com/dove/test/CacheThreadPool.java
@@ -0,0 +1,68 @@
+package com.dove.test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+class User {
+	int userId;
+	String username;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public User(int userId, String username) {
+		super();
+		this.userId = userId;
+		this.username = username;
+	}
+
+}
+
+public class CacheThreadPool {
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		Future<User> future = exec.submit(new Callable<User>() {
+			@Override
+			public User call() throws Exception {
+				// TODO Auto-generated method stub
+				System.out.println("before sleep");
+				User user = new User(1, "zjg");
+				Thread.sleep(500);
+				System.out.println("after sleep");
+				return user;
+			}
+		});
+
+		try {
+			future.get(501, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			System.out.println("timeout\n");
+			e.printStackTrace();
+		}
+	}
+}
