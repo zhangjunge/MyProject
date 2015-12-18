649a15d97b9b07ca6f959da0b4fef4b2358ea712
zhangjunge <593450804@qq.com>
2015-12-18 11:21:04 +0800

    Update MergeTest.java
    github增加方法

--- src/com/dove/test/MergeTest.java
+++ src/com/dove/test/MergeTest.java
@@ -6,6 +6,12 @@ public class MergeTest {
 		System.out.println("i edit it on local!");
 		System.out.print("i add something which conflict to github ");
 		System.out.println("i edit it on github");
+		githubmethod();
 	}
+	
+	static private int githubmethod(){
+    	System.out.println("github增加方法");
+    	return 1;
+    }
 
 }
