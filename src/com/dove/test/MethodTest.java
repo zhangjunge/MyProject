18f5c672a6105cca9dc344dbce698e2d58fd72f6
张俊格 <593450804@qq.com>
2016-01-06 23:37:49 +0800

    void

--- src/com/dove/test/MethodTest.java
+++ src/com/dove/test/MethodTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.dove.test;
 
 import java.math.BigDecimal;
@@ -10,7 +10,8 @@ public class MethodTest {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public server(){
+	public void
+	server(){
 		
 	}
 	public static void main(String[] args) {
@@ -31,37 +32,3 @@ public class MethodTest {
 	}
 
 }
-=======
-package com.dove.test;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
-public class MethodTest {
-
-	public MethodTest() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public server(){
-		
-	}
-	public static void main(String[] args) {
-		
-		String currentShouldRepayTotalStr = "110.0952";
-		BigDecimal currentShouldRepayTotal = BigDecimal.ZERO;
-		 currentShouldRepayTotal = new BigDecimal(currentShouldRepayTotalStr).multiply(new BigDecimal("100"))
-                 .setScale(0, RoundingMode.HALF_EVEN);//四舍五入
-		 System.out.println(currentShouldRepayTotal);
-		 BigDecimal bd = new BigDecimal(1.0);
-		 BigDecimal sum  = currentShouldRepayTotal.add(bd);
-		 System.out.println(sum);
-		if( bd.compareTo(sum)<0){
-			System.out.println("bd<sum");
-		}
-		 
-		 
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/zhangjunge/MyProject.git
