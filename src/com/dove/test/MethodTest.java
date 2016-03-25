f8104aee5bffc7592fb201c07d6510c0e94e4028
张俊格 <dove.zhang@vipshop.com>
2016-03-25 09:30:08 +0800

    methodTest

--- src/com/dove/test/MethodTest.java
+++ src/com/dove/test/MethodTest.java
@@ -3,34 +3,48 @@ package com.dove.test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.HashMap;
+
+import com.alibaba.fastjson.JSONObject;
 
 public class MethodTest {
 
 	public MethodTest() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void local(){}
-	
-	public void
-	server(){
-		
+
+	public void server() {
+
 	}
+
 	public static void main(String[] args) {
+
+		String a = "110.0952";
+		BigDecimal b = BigDecimal.ZERO;
+		b = new BigDecimal(a).multiply(new BigDecimal("100")).setScale(0, RoundingMode.HALF_EVEN);// 四舍五入
+		BigDecimal c = new BigDecimal(a).multiply(new BigDecimal("100")).setScale(0, RoundingMode.HALF_EVEN);// 四舍五入
 		
-		String currentShouldRepayTotalStr = "110.0952";
-		BigDecimal currentShouldRepayTotal = BigDecimal.ZERO;
-		 currentShouldRepayTotal = new BigDecimal(currentShouldRepayTotalStr).multiply(new BigDecimal("100"))
-                 .setScale(0, RoundingMode.HALF_EVEN);//四舍五入
-		 System.out.println(currentShouldRepayTotal);
-		 BigDecimal bd = new BigDecimal(1.0);
-		 BigDecimal sum  = currentShouldRepayTotal.add(bd);
-		 System.out.println(sum);
-		if( bd.compareTo(sum)<0){
-			System.out.println("bd<sum");
+		if(b.compareTo(c)==0){
+			System.out.println("b==c  "+b);
 		}
-		 
-		 
+		BigDecimal  repayment = new BigDecimal("0.000");
+		
+		if(repayment.compareTo(BigDecimal.ZERO)==0){
+			System.out.println("无欠款");
+		}
+		System.out.println(repayment);
+		System.out.println(BigDecimal.ZERO);
+		
+		String s = "{\"userId\":\"123\"}";
+		JSONObject jsonObject = JSONObject.parseObject(s);
+		jsonObject.put("mktPlan", "短信");
+		String object = JSONObject.toJSONString(jsonObject);
+		System.out.println(object);
+		
+		
+		
+
+		
 	}
 
 }
