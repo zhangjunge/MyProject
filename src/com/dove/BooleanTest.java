5c1a83aaf07a2cf229e31fa269d3e772bd3448b8
张俊格 <dove.zhang@vipshop.com>
2016-05-17 10:00:13 +0800

    booleantest

---
+++ src/com/dove/BooleanTest.java
@@ -0,0 +1,21 @@
+package com.dove;
+
+/**
+ * Created by dove.zhang on 2016/5/13.
+ */
+public class BooleanTest {
+
+    public static int get(){
+        return 1;
+    }
+
+    public static void main(String[] args){
+        Boolean flag = true;
+
+        Integer i = 9;
+        String s = i.toString();
+        System.out.print(s);
+
+
+    }
+}
